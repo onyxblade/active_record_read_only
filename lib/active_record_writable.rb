0f2af8af56af962942dc91657968f2e1a76739f3
merely <git@merely.ca>
2026-05-22 02:28:02 -0300

    init

---
+++ lib/active_record_writable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "active_record_writable/version"
+
+module ActiveRecordWritable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_record_writable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActiveRecordWritable
+  VERSION = "0.1.0"
+end
